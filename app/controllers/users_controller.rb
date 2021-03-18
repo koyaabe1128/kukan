791b08e700cbd0c439ceaa8955772ef1130cbdd5
Ubuntu <ubuntu@ip-172-31-17-11.ap-northeast-1.compute.internal>
2021-03-18 16:06:26 +0000

    Add UserModel UsersController veiws/users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update(user_profile_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+  
+  private
+  
+  #Strong Parameter
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  def user_profile_params
+    params.require(:user).permit(:name, :image, :introduction, :genre, :prefecture)
+  end
+end
